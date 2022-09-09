293b4b3047043f5e2b68dc1012a4ab2c6384e517
Никита <nikityary@mail.ru>
2022-09-09 16:06:51 +0300

    Matrix

---
+++ Matpica/Matrix.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matpica
+{
+    internal class Matrix
+    {
+        public int k, t, i, j;
+        double[,] DoubleArray;
+        int n, m;
+
+        public Matrix(int n, int m)
+        {
+            this.n = n;
+            this.m = m;
+            DoubleArray = new double[n, m];
+        }
+
+        //ПЕРВАЯ МАТРИЦА
+        // Ввести элементы
+        public void EnterElements()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write("Введите элемнет [{0},{1}] :  ", i, j);
+                    DoubleArray[i, j] = Double.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        // Вывести элементы матрицы на экран
+        public void PrintMatrix()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(DoubleArray[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //ВТОРАЯ МАТРИЦА
+        // Ввести элементы
+        public void EnterElements2()
+        {
+            for (int k = 0; k < n; k++)
+            {
+                for (int t = 0; t < m; t++)
+                {
+                    Console.Write("Введите элемнет [{0},{1}] :  ", k, t);
+                    DoubleArray[k, t] = Double.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        // Вывести элементы матрицы на экран
+        public void PrintMatrix2()
+        {
+            for (int k = 0; k < n; k++)
+            {
+                for (int t = 0; t < m; t++)
+                {
+                    Console.Write(DoubleArray[k, t] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //Сложение матриц 1 и 2
+        public void Plus()
+        {
+            for (int z = 0; z < n; z++)
+            {
+                for (int c = 0; c < m; c++)
+                {
+                    int[,] mat3 = new int[z, c];
+                     DoubleArray[z, c] = DoubleArray[k, t] + DoubleArray[i, j];
+                }
+                Console.WriteLine();
+            }
+        }
+        // Вывести результат на экран
+        public void PrintMatrix3()
+        {
+            for (int z = 0; z < n; z++)
+            {
+                for (int c = 0; c < m; c++)
+                {
+                    Console.Write(DoubleArray[z, c] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // Умножение
+        public double Multiply
+        {
+            set
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        DoubleArray[i, j] *= value;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Matpica/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matpica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix mat1 = new Matrix(3, 3);
+            mat1.EnterElements(); //Ввести элементы матрицы
+            Console.WriteLine(new string('-', 35));
+            mat1.PrintMatrix(); //Отобразить матрицу
+            Console.WriteLine(new string('-', 35));
+
+
+            Matrix mat2 = new Matrix(3, 3);
+            mat2.EnterElements2(); //Ввести элементы матрицы 2
+            Console.WriteLine(new string('-', 35));
+            mat2.PrintMatrix2(); //Отобразить матрицу 2
+            Console.WriteLine(new string('-', 35));
+
+            Matrix mat3 = new Matrix(3, 3);
+            mat3.PrintMatrix3(); //Отобразить матрицу 2
+            Console.WriteLine(new string('-', 35));
+
+            Console.Write("Введите множитель: ");
+            mat1.Multiply = Double.Parse(Console.ReadLine());
+            mat1.PrintMatrix(); //Отобразить матрицу
+            Console.WriteLine(new string('-', 35));
+            Console.ReadKey();
+        }
+    }
+}
